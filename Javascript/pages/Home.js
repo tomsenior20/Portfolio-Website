46ea386f9b3e7cb6a52463bf9f93083b229232a9
tomsenior20 <tomsenior20@gmail.com>
2022-10-09 16:17:08 +0100

    added hide show feature for work experience

--- Javascript/pages/Home.js
+++ Javascript/pages/Home.js
@@ -1,7 +1,13 @@
 window.addEventListener('load', () => {
+    console.log(document.readyState);
     setInterval(() => {
         getTime();
     }, 1000);
+
+    var revealButton = document.getElementById('revealExperienceButton').addEventListener('click', () => {
+        var showExperienceContainer = document.getElementById('workExperienceContainer').style;
+        showExperience(showExperienceContainer);
+    });
 });
 
 getTime = () => {
@@ -16,3 +22,14 @@ getTime = () => {
     var ampm = (hours < 12 ? 'AM' : 'PM');
     document.getElementById('timeContainer').innerHTML = `<p class='introText text-center my-3 py-2'>${hours}:${minutes}:${seconds}:${ampm}</p>`;
 }
+
+showExperience = (showExperienceContainer) => {
+    let buttonText = document.getElementById('revealExperienceButton');
+    if (showExperienceContainer.display == 'none') {
+        showExperienceContainer.display = 'flex';
+        buttonText.innerHTML = 'Close Experience';
+    } else {
+        showExperienceContainer.display = 'none';
+        buttonText.innerHTML = 'Reveal Experience';
+    }
+};
